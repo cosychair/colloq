a4ec407734ab1d612e90909a7f27d29830781323
cosychair <37767339+cosychair@users.noreply.github.com>
2018-04-09 23:01:33 +0300

    Add files via upload

--- colloquium/colloquium/Input/inputN.cpp
+++ colloquium/colloquium/Input/inputN.cpp
@@ -2,23 +2,38 @@
 #include"../N/longNat.h"
 #include <iostream>
 #include <iomanip>
+#include "../Output/outputCom.h"
 using namespace std;
 
 LNGNT readN() {
 	LNGNT a;
 	char k;
 	int* tmp;
+	a.A = NULL;
 	a.n = 0;
-	while (cin.get(k)) {
-		if (k >= '0'&&k <= '9') {
-			tmp = (int*)realloc(a.A, (a.n+1)*sizeof(int));
+	int err = 0;
+	do
+	{
+		scanf_s("%c", &k);
+		if (a.n) {
+			tmp = (int*)realloc(a.A, (a.n + 1) * sizeof(int));
 			if (tmp == NULL) {
 				cout << "MEMORY ENDED JUST NOW, NUMBER IS TOO LONG" << endl;
 				return a;
 			}
 			a.A = tmp;
-			a.A[a.n] = k;
-			a.n++;
 		}
-	}
+		else
+			a.A = (int*)malloc(sizeof(int));
+		err = a.n == 0 && k == '\n';
+		if (!err)
+		{
+			if (k > 47 && k < 58)
+			{
+				*(a.A + a.n) = k - '0';
+				a.n++;
+			}
+		}
+	} while (err || k != '\n');
+	return a;
 }
