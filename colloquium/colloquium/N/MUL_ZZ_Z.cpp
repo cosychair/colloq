22b609e9568f255e537ccf2845bd572ca8072a36
cosychair <37767339+cosychair@users.noreply.github.com>
2018-04-11 22:51:55 +0300

    Add files via upload

---
+++ colloquium/colloquium/N/MUL_ZZ_Z.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <iostream>
+#include "longInteger.h"
+#include "../Z/longIntFunctions.h"
+#include "../N/longNatFunctions.h"
+#include "../N/helpFunctions.h"
+using namespace std;
+
+LNGINT MUL_ZZ_Z(LNGINT ln, LNGINT lt)
+    {
+        LNGINT ttr;
+        LNGNT lx;
+        int error = 0;
+        if(ln.A && lt.A)
+            {
+                LNGNT lnN,ltN;
+                lnN = ABS_Z_N(ln);      
+                ltN = ABS_Z_N(lt);     
+                if(lnN.A && ltN.A)
+                    {
+                        lx = MUL_NN_N(lnN, ltN); 
+                        ttr.sign = 0;
+                        ttr.n = lx.n;
+                        ttr.A = (int*) malloc(ttr.n*sizeof(int));
+                        for(int i = 0; i < lx.n; ++i)
+                            *(ttr.A + i) = *(lx.A+i);
+                        if(ln.sign != lt.sign)
+							ttr = MUL_ZM_Z(ttr); 
+						if (ttr.A[0] == 0 )
+						{
+							ttr.n = 1;
+							ttr.sign = 0;
+						}
+						else;
+                    }
+                else
+                    error = 1;
+            }
+        else
+            error = 1;
+        
+        if(error == 1)
+            cout << "Ошибка, числа не существует!" << endl;
+        
+        return ttr;
+    }
