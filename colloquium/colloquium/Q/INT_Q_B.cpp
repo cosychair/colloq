79f2377db90592b2950c44838c3375a5e32ba9a6
cosychair <37767339+cosychair@users.noreply.github.com>
2018-04-11 23:10:56 +0300

    Add files via upload

--- colloquium/colloquium/Q/INT_Q_B.cpp
+++ colloquium/colloquium/Q/INT_Q_B.cpp
@@ -8,7 +8,10 @@ int INT_Q_B(FRCT num)
 {
 	int ans = 0;
 	if (num.den.A && num.num.A)
-		ans = *(num.den.A) == 1 && num.den.n == 1;
+	{
+		num = RED_Q_Q(num);
+		ans = num.den.A[0] == 1 && num.den.n == 1;
+	}
 	else
 	{
 		std::cout << "Ошибка, число не существует!" ;
