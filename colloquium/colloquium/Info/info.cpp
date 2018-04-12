0235eec2b78d8f8a3481b3cb4d4fd97e89992801
cosychair <37767339+cosychair@users.noreply.github.com>
2018-04-13 01:46:28 +0300

    Add files via upload

--- colloquium/colloquium/Info/info.cpp
+++ colloquium/colloquium/Info/info.cpp
@@ -12,6 +12,14 @@
 #include "../Z/longIntFunctions.h"
 #include "../Input/inputZ.h"
 #include "../Output/outputZ.h"
+#include "../Q/longFraction.h"
+#include "../Q/longFracFunctions.h"
+#include "../Output/outputQ.h"
+#include "../Input/inputQ.h"
+#include "../Input/inputP.h"
+#include "../Output/outputP.h"
+#include "../P/longPoly.h"
+#include "../P/longPolyFunctions.h"
 #include "info.h"
 
 using namespace std;
@@ -274,5 +282,258 @@ void infoZ()
 		}
 		system("cls");
 		chooseFunctionZ(number);
+	}
+}
+
+int chooseFunctionQ(int number) {
+	FRCT a, b;
+	LNGINT z;
+	a.den.A = a.num.A = b.den.A = b.num.A = NULL;
+	/*int number = 0;
+	do {
+	cin >> number;*/
+	switch (number) {
+	case 1:
+		printFR();
+		a = readQ();
+		printQ(RED_Q_Q(a));
+		break;
+	case 2:
+		printFR();
+		a = readQ();
+		if (INT_Q_B(a) == 1)
+			cout << "This isn`t a fraction." << endl;
+		else
+			cout << "This is a fraction." << endl;
+		break;
+	case 3:
+		printIN();
+		z = readZ();
+		printQ(TRANS_Z_Q(z));
+		break;
+	case 4:
+		printFR();
+		a = readQ();
+		printZ(TRANS_Q_Z(a));
+		break;
+	case 5:
+		printFR();
+		a = readQ();
+		printFR();
+		b = readQ();
+		printQ(ADD_QQ_Q(a, b));
+		break;
+	case 6:
+		printFR();
+		a = readQ();
+		printFR();
+		b = readQ();
+		printQ(SUB_QQ_Q(a, b));
+		break;
+	case 7:
+		printFR();
+		a = readQ();
+		printFR();
+		b = readQ();
+		printQ(MUL_QQ_Q(a, b));
+		break;
+	case 8:
+		printFR();
+		a = readQ();
+		printFR();
+		b = readQ();
+		printQ(DIV_QQ_Q(a, b));
+		break;
+	default: printError(); number = 0; break;
+	}
+	/*} while (number == 0);*/
+	cin.get();
+	cin.get();
+	freeQ(&a);
+	freeQ(&b);
+	return number;
+}
+
+void infoQ()
+{
+	string temp = "";
+	int i = 0, number = 0, st = 1;
+	cin >> temp;
+	i = temp.length();
+	if (temp[0] == 'i')
+	{
+		for (int j = i - 1; j != 3; --j)
+		{
+			number = number + (temp[j] - 48) * st;
+			st = st * 10;
+		}
+		system("cls");
+		showInfoMenuQ(number);
+		chooseFunctionQ(number);
+	}
+	else
+	{
+		for (int j = i - 1; j != -1; --j)
+		{
+			number = number + ((int)temp[j] - 48) * st;
+			st = st * 10;
+		}
+		system("cls");
+		chooseFunctionQ(number);
+	}
+}
+
+int chooseFunctionP(int number) {
+	PLNM a, b;
+	FRCT q;
+	int d;
+	char c;
+	/*int number = 0;
+	do {
+	cin >> number;*/
+	switch (number) {
+	case 1:
+		printPLN();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		a = readP();
+		printPLN();
+		b = readP();
+		printP(ADD_PP_P(a, b));
+		break;
+	case 2:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printPLN();
+		b = readP();
+		printP(SUB_PP_P(a, b));
+		break;
+	case 3:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printFR();
+		q = readQ();
+		printP(MUL_PQ_P(a, q));
+		break;
+	case 4:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printIN();
+		cin >> d;
+		printP(MUL_Pxk_P(a, d));
+		break;
+	case 5:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printQ(LED_P_Q(a));
+		break;
+	case 6:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		cout << "\n" << DEG_P_N(a) << endl;
+		break;
+	case 7:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printP(FAC_P_Q(a));
+		break;
+	case 8:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printPLN();
+		b = readP();
+		printP(MUL_PP_P(a, b));
+		break;
+		/*case 9:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printPLN();
+		b = readP();
+		printP(DIV_PP_P(a, b));
+		break;
+		case 10:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printPLN();
+		b = readP();
+		printP(MOD_PP_P(a, b));
+		break;
+		case 11:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printPLN();
+		b = readP();
+		printP(GCF_PP_P(a, b));
+		break;*/
+	case 12:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printP(DER_P_P(a));
+		break;
+		/*case 13:
+		printPLN();
+		a = readP();
+		c = ' ';//fix print
+		while (std::cin.get(c) && c != '\n');
+		printP(NMR_P_P(a));
+		break;*/
+	default: printError(); number = 0; break;
+	}
+	/*} while (number == 0);*/
+	cin.get();
+	cin.get();
+	//freeP(&a);
+	//freeP(&b);
+	return number;
+}
+
+void infoP()
+{
+	string temp = "";
+	int i = 0, number = 0, st = 1;
+	cin >> temp;
+	i = temp.length();
+	if (temp[0] == 'i')
+	{
+		for (int j = i - 1; j != 3; --j)
+		{
+			number = number + (temp[j] - 48) * st;
+			st = st * 10;
+		}
+		system("cls");
+		showInfoMenuP(number);
+		chooseFunctionP(number);
+	}
+	else
+	{
+		for (int j = i - 1; j != -1; --j)
+		{
+			number = number + ((int)temp[j] - 48) * st;
+			st = st * 10;
+		}
+		system("cls");
+		chooseFunctionP(number);
 	}
 }
--- colloquium/colloquium/Info/info.h
+++ colloquium/colloquium/Info/info.h
@@ -1,6 +1,15 @@
 #pragma once
 #ifndef INFO_H
 #define INFO_H
+
+int chooseFunctionN(int number);
+
+int chooseFunctionZ(int number);
+
+int chooseFunctionQ(int number);
+
+int chooseFunctionP(int number);
+
 /*
 
 Функция обработки входных значений для натуральных чисел
@@ -8,9 +17,9 @@
 */
 void infoN();
 
-int chooseFunctionN(int number);
-
-int chooseFunctionZ(int number);
-
 void infoZ();
+
+void infoQ();
+
+void infoP();
 #endif
